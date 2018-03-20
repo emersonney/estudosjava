7e5c9bab8c306df83efe288d5b0d0df6fd54fcef
emersonney <emerson.farias@gmail.com>
2018-03-20 11:07:10 -0300

    aula dia 20/03

--- estudosjava/curso-java-oo/src/aula23/Aula.java
+++ estudosjava/curso-java-oo/src/aula23/Aula.java
@@ -3,30 +3,29 @@ package aula23;
 public class Aula {
 
 	public static void main(String[] args) {
-		
+
 		teste01();
-		 
+
 	}
-	
+
 	private static void teste01() {
-		
+
 		Funcionario f1 = new Funcionario() {
-			
+
 			@Override
 			public double remuneracao() {
-				// TODO Auto-generated method stub 
+				// TODO Auto-generated method stub
 				return 800 + 900;
 			}
 		};
-		System.out.println( f1.remuneracao() );
+		System.out.println(f1.remuneracao());
 		System.out.println("------------------------");
-		
-		
-		Funcionario f2 = () -> { return 800 + 900; };
+
+		Funcionario f2 = () -> {
+			return 800 + 900;
+		};
 		f2.imprime();
-		
-		
+
 	}
-	
-	
+
 }
